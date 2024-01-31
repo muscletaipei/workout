e55b979c1d4bd2f3c7a75e89542c428f692362c6
MSI\joelin <muscletaipei@gmail.com>
2024-01-31 15:59:15 +0800

    V1.0: Update RPE table

--- Workout/MainWindow.xaml.cs
+++ Workout/MainWindow.xaml.cs
@@ -14,18 +14,31 @@ namespace OneRMEstimation
         {
             try
             {
-                // 取得輸入的訓練重量和次數
-                double weight = Convert.ToDouble(weightTextBox.Text);
-                int reps = Convert.ToInt32(repsTextBox.Text);
+                // 取得實際訓練重量和實際次數
+                double actualWeight = Convert.ToDouble(actualWeightTextBox.Text);
+                int actualReps = Convert.ToInt32(actualRepsTextBox.Text);
 
-                // 估算1RM
-                double estimated1RM = Estimate1RMWeight(weight, reps);
+                // 計算1RM
+                double calculated1RM = Calculate1RM(actualWeight, actualReps);
 
-                // 轉換為整數
-                int int1RM = Convert.ToInt32(estimated1RM);
+                // 取得預計訓練次數
+                int predictedReps = Convert.ToInt32(predictedRepsTextBox.Text);
 
-                // 顯示結果
-                resultTextBlock.Text = $"根據您的訓練，估算的1RM重量為：{int1RM} kg";
+                // 列出不同RPE強度對應的1RM重量
+                resultTextBlock.Text = "RPE強度對應的訓練重量為：\n";
+
+                double[] specifiedRPEs = { 10, 9.5, 9, 8.5, 8, 7.5, 7, 5, 3 };
+
+                foreach (double rpe in specifiedRPEs)
+                {
+                    double percentage = EstimatePercentage(rpe, predictedReps);
+                    double estimated1RM = calculated1RM * percentage;
+
+                    // 轉換為整數
+                    int int1RM = Convert.ToInt32(estimated1RM);
+
+                    resultTextBlock.Text += $"RPE {rpe}: {int1RM} kg\n";
+                }
             }
             catch (Exception ex)
             {
@@ -33,34 +46,174 @@ namespace OneRMEstimation
             }
         }
 
-        private double Estimate1RMWeight(double weight, int reps)
+        private double Calculate1RM(double weight, int reps)
         {
-            // 根據次數的百分比估算1RM
+            // 使用您之前提供的1RM公式
             double percentage = 0;
 
-            if (reps == 1)
-                percentage = 1.0;
-            else if (reps == 2)
-                percentage = 0.96;
-            else if (reps == 3)
-                percentage = 0.92;
-            else if (reps == 4)
-                percentage = 0.89;
-            else if (reps == 5)
-                percentage = 0.86;
-            else if (reps == 6)
-                percentage = 0.84;
-            else if (reps == 7)
-                percentage = 0.81;
-            else if (reps == 8)
-                percentage = 0.78;
-            else if (reps == 9)
-                percentage = 0.76;
-            else if (reps == 10)
-                percentage = 0.74;
+            switch (reps)
+            {
+                case 1: percentage = 1.00; break;
+                case 2: percentage = 0.96; break;
+                case 3: percentage = 0.92; break;
+                case 4: percentage = 0.89; break;
+                case 5: percentage = 0.86; break;
+                case 6: percentage = 0.84; break;
+                case 7: percentage = 0.81; break;
+                case 8: percentage = 0.78; break;
+                case 9: percentage = 0.76; break;
+                case 10: percentage = 0.74; break;
+            }
 
-            // 直接轉換為整數部分
-            return (int)((1/ percentage) * weight);
+            return (1 / percentage) * weight;
+        }
+
+        private double EstimatePercentage(double rpe, int reps)
+        {
+            // 根據RPE強度和預計訓練次數估算百分比
+            double percentage = 0;
+
+            switch (rpe)
+            {
+                case 10:
+                    switch (reps)
+                    {
+                        case 1: percentage = 1.00; break;
+                        case 2: percentage = 0.96; break;
+                        case 3: percentage = 0.92; break;
+                        case 4: percentage = 0.89; break;
+                        case 5: percentage = 0.86; break;
+                        case 6: percentage = 0.84; break;
+                        case 7: percentage = 0.81; break;
+                        case 8: percentage = 0.78; break;
+                        case 9: percentage = 0.76; break;
+                        case 10: percentage = 0.74; break;
+                    }
+                    break;
+                // 其他RPE強度的百分比計算類似，可以自行擴展
+                case 9.5:
+                    switch (reps)
+                    {
+                        case 1: percentage = 0.98; break;
+                        case 2: percentage = 0.94; break;
+                        case 3: percentage = 0.91; break;
+                        case 4: percentage = 0.88; break;
+                        case 5: percentage = 0.85; break;
+                        case 6: percentage = 0.82; break;
+                        case 7: percentage = 0.80; break;
+                        case 8: percentage = 0.77; break;
+                        case 9: percentage = 0.75; break;
+                        case 10: percentage = 0.72; break;
+                    }
+                    break;
+                case 9:
+                    switch (reps)
+                    {
+                        case 1: percentage = 0.96; break;
+                        case 2: percentage = 0.92; break;
+                        case 3: percentage = 0.89; break;
+                        case 4: percentage = 0.86; break;
+                        case 5: percentage = 0.84; break;
+                        case 6: percentage = 0.81; break;
+                        case 7: percentage = 0.79; break;
+                        case 8: percentage = 0.76; break;
+                        case 9: percentage = 0.74; break;
+                        case 10: percentage = 0.71; break;
+                    }
+                    break;
+                case 8.5:
+                    switch (reps)
+                    {
+                        case 1: percentage = 0.94; break;
+                        case 2: percentage = 0.91; break;
+                        case 3: percentage = 0.88; break;
+                        case 4: percentage = 0.85; break;
+                        case 5: percentage = 0.82; break;
+                        case 6: percentage = 0.80; break;
+                        case 7: percentage = 0.77; break;
+                        case 8: percentage = 0.75; break;
+                        case 9: percentage = 0.72; break;
+                        case 10: percentage = 0.69; break;
+                    }
+                    break;
+                case 8:
+                    switch (reps)
+                    {
+                        case 1: percentage = 0.92; break;
+                        case 2: percentage = 0.89; break;
+                        case 3: percentage = 0.86; break;
+                        case 4: percentage = 0.84; break;
+                        case 5: percentage = 0.81; break;
+                        case 6: percentage = 0.79; break;
+                        case 7: percentage = 0.76; break;
+                        case 8: percentage = 0.74; break;
+                        case 9: percentage = 0.71; break;
+                        case 10: percentage = 0.68; break;
+                    }
+                    break;
+                case 7.5:
+                    switch (reps)
+                    {
+                        case 1: percentage = 0.91; break;
+                        case 2: percentage = 0.88; break;
+                        case 3: percentage = 0.85; break;
+                        case 4: percentage = 0.82; break;
+                        case 5: percentage = 0.80; break;
+                        case 6: percentage = 0.77; break;
+                        case 7: percentage = 0.75; break;
+                        case 8: percentage = 0.72; break;
+                        case 9: percentage = 0.69; break;
+                        case 10: percentage = 0.67; break;
+                    }
+                    break;
+                case 7:
+                    switch (reps)
+                    {
+                        case 1: percentage = 0.89; break;
+                        case 2: percentage = 0.86; break;
+                        case 3: percentage = 0.84; break;
+                        case 4: percentage = 0.81; break;
+                        case 5: percentage = 0.79; break;
+                        case 6: percentage = 0.76; break;
+                        case 7: percentage = 0.74; break;
+                        case 8: percentage = 0.71; break;
+                        case 9: percentage = 0.68; break;
+                        case 10: percentage = 0.65; break;
+                    }
+                    break;
+                case 5:
+                    switch (reps)
+                    {
+                        case 1: percentage = 0.83; break;
+                        case 2: percentage = 0.80; break;
+                        case 3: percentage = 0.78; break;
+                        case 4: percentage = 0.75; break;
+                        case 5: percentage = 0.73; break;
+                        case 6: percentage = 0.70; break;
+                        case 7: percentage = 0.68; break;
+                        case 8: percentage = 0.65; break;
+                        case 9: percentage = 0.62; break;
+                        case 10: percentage = 0.59; break;
+                    }
+                    break;
+                case 3:
+                    switch (reps)
+                    {
+                        case 1: percentage = 0.77; break;
+                        case 2: percentage = 0.74; break;
+                        case 3: percentage = 0.72; break;
+                        case 4: percentage = 0.69; break;
+                        case 5: percentage = 0.67; break;
+                        case 6: percentage = 0.64; break;
+                        case 7: percentage = 0.62; break;
+                        case 8: percentage = 0.59; break;
+                        case 9: percentage = 0.56; break;
+                        case 10: percentage = 0.53; break;
+                    }
+                    break;
+            }
+
+            return percentage;
         }
     }
 }
